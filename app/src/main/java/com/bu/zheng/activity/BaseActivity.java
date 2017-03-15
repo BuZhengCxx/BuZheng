f4480cae6d905e7cf2c7848a0c454f410080d632
hzchenxiaoxiong <hzchenxiaoxiong@crop.netease.com>
2017-03-15 17:53:24 +0800

    nothing

---
+++ app/src/main/java/com/bu/zheng/activity/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.bu.zheng.activity;
+
+/**
+ * Created by BuZheng on 2017/3/15.
+ */
+
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/bu/zheng/fragment/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.bu.zheng.fragment;
+
+/**
+ * Created by BuZheng on 2017/3/15.
+ */
+
+public class BaseFragment {
+}
